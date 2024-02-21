7f519cf2fc7dfe443975bab7c84bafca307d1680
Luke Skinner <laskinner@gmail.com>
2024-02-21 17:56:29 +0100

    Testing delpoy with changing some basic text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to Dromos!</h1>
       <Button>Click me</Button>
     </div>
   );
