2cd275019acbf54fbf04dde4b284422249f93aa5
Luke Skinner <laskinner@gmail.com>
2024-02-21 04:24:28 +0100

    Finished and successfully tested shadcn/ui set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
