f1abc964964e22cc725128b06cca09b39848f730
Luke Skinner <laskinner@gmail.com>
2024-02-21 18:20:29 +0100

    Added navbar component from proj directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Welcome to Dromos!</h1>
       <Button>Click me</Button>
     </div>
