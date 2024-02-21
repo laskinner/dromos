1bd084a2b721f9f5046c01b9d198635a8493ffca
Luke Skinner <laskinner@gmail.com>
2024-02-21 18:20:03 +0100

    Added Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const NavBar: React.FC = () => {
+  return (
+    <nav>
+      {/* Navigation links or branding can go here */}
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </nav>
+  );
+};
+
+export default NavBar;
