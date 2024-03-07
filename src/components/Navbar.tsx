6a5aeda84a93ec134e085f4f53470741e41478a2
Luke Skinner <laskinner@gmail.com>
2024-03-07 05:29:15 +0100

    Added favicon and logo, collected font awesome icons and placed them in public

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,15 @@ const NavBar: React.FC = () => {
   return (
     <nav>
       {/* Navigation links or branding can go here */}
+      <img
+        src="/dromos-high-resolution-logo-transparent.png"
+        alt="Dromos Logo"
+      />{" "}
+      {/* Referencing logo */}
       <a href="/">Home</a>
+      <i className="fa-solid fa-house"></i>
+      <i className="fa-solid fa-user-plus"></i>
+      <i className="fa-regular fa-circle-nodes"></i>
       <a href="/about">About</a>
     </nav>
   );
