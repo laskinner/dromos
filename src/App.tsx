8468db68e3224a26856014fc0a604ea43e9489e4
Luke Skinner <laskinner@gmail.com>
2024-03-08 01:42:12 +0100

    Created all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "@/components/Navbar";
+import CreateAccount from "@/components/CreateAccount";
+import Home from "@/components/Home";
+import EditProfile from "@/components/EditProfile";
+import CreateNode from "@/components/CreateNode";
+import CreateNodeGraph from "@/components/CreateNodeGraph";
+import GraphView from "@/components/GraphView";
+import LogIn from "@/components/LogIn";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Welcome to Dromos!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/create-node" element={<CreateNode />} />
+        <Route path="/create-node-graph" element={<CreateNodeGraph />} />
+        <Route path="/graph-view" element={<GraphView />} />
+        <Route path="/log-in" element={<LogIn />} />
+        {/* Add more routes here as needed */}
+      </Routes>
     </div>
   );
 }
