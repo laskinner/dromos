9513670d44755c764cf253fe64d185647b0782db
Luke Skinner <laskinner@gmail.com>
2024-03-09 13:39:00 +0100

    Updated icons to be a different color when highlighted

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { NavLink } from "react-router-dom"; // Change from Link to NavLink
 
 const NavBar: React.FC = () => {
   return (
     <nav className="flex items-center justify-between bg-transparent py-4 px-6">
       <div className="flex-1">
-        <Link to="/" className="group">
+        <NavLink to="/" className="group">
           <img
             src="/dromos-high-resolution-logo-transparent.png"
             alt="Dromos Logo"
             className="h-20"
           />
-        </Link>
+        </NavLink>
       </div>
       <div className="flex-1 flex justify-center">
-        <Link to="/" className="group mx-2">
-          <i className="fa-solid fa-house text-3xl hover:text-gray-700"></i>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `group mx-2 ${
+              isActive ? "text-blue-600" : "text-stone-500"
+            } text-3xl hover:text-gray-700`
+          }
+        >
+          <i className="fa-solid fa-house"></i>
           <span className="absolute mt-6 px-2 py-1 text-xs text-white bg-black rounded-md opacity-0 group-hover:opacity-100">
             Home
           </span>
-        </Link>
-        <Link to="/graph-view" className="group mx-2">
-          <i className="fa-regular fa-circle-nodes text-3xl hover:text-gray-700"></i>
+        </NavLink>
+        <NavLink
+          to="/graph-view"
+          className={({ isActive }) =>
+            `group mx-2 ${
+              isActive ? "text-blue-600" : "text-stone-500"
+            } text-3xl hover:text-gray-700`
+          }
+        >
+          <i className="fa-regular fa-circle-nodes"></i>
           <span className="absolute mt-6 px-2 py-1 text-xs text-white bg-black rounded-md opacity-0 group-hover:opacity-100">
             View Node Graphs
           </span>
-        </Link>
+        </NavLink>
       </div>
       <div className="flex-1 text-right">
-        <Link to="/create-account" className="group">
-          <i className="fa-solid fa-user-plus text-3xl hover:text-gray-700"></i>
+        <NavLink
+          to="/create-account"
+          className={({ isActive }) =>
+            `group ${
+              isActive ? "text-blue-600" : "text-stone-500"
+            } text-3xl hover:text-gray-700`
+          }
+        >
+          <i className="fa-solid fa-user-plus"></i>
           <span className="absolute mt-6 px-2 py-1 text-xs text-white bg-black rounded-md opacity-0 group-hover:opacity-100">
             Create Account
           </span>
-        </Link>
+        </NavLink>
       </div>
     </nav>
   );
