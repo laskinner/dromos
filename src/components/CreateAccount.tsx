c32830df958631c4250018cfac4cef4330b7a750
Luke Skinner <laskinner@gmail.com>
2024-03-10 17:29:18 +0100

    Finished setting up createaccount component and form, prepared for additional fields

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { SheetClose } from "@/components/ui/sheet";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  SheetClose,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
 
 const CreateAccount: React.FC = () => {
   return (
     <>
+      <SheetHeader>
+        <SheetTitle>Create profile</SheetTitle>
+        <SheetDescription>Click save when you're done.</SheetDescription>
+      </SheetHeader>
       <div className="grid gap-4 py-4">
         {/* Form fields here */}
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="username" className="text-right">
+          <Label htmlFor="name" className="text-right">
             Username
           </Label>
-          <Input id="username" placeholder="Username" className="col-span-3" />
-          {/* Repeat for other fields */}
+          <Input id="name" placeholder="Username" className="col-span-3" />
+          {/* Include other fields here, such as content for bio */}
         </div>
-        {/* More fields can be added here */}
+        {/* More fields can be added here, such as image and email */}
       </div>
       <SheetClose asChild>
-        <Button type="submit">Save changes</Button>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
       </SheetClose>
     </>
   );
