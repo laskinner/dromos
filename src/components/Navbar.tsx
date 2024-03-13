84b91a13e44d14b55f82f9656c9ca4031aaec28a
Luke Skinner <laskinner@gmail.com>
2024-03-13 04:18:27 +0100

    Created login and create account toggle functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom"; // Change from Link to NavLink
 import {
   Tooltip,
@@ -7,6 +7,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import CreateAccount from "@/components/CreateAccount";
+import LogIn from "@/components/LogIn";
 import {
   Sheet,
   SheetClose,
@@ -20,6 +21,10 @@ import {
 import { Button } from "@/components/ui/button";
 
 const NavBar: React.FC = () => {
+  const [isCreatingAccount, setIsCreatingAccount] = useState(true); // Default to showing the create account form
+
+  const toggleForm = () => setIsCreatingAccount(!isCreatingAccount);
+
   const handleClickSubmit = () => {
     const hiddenSubmitButton = document.getElementById("hidden-submit");
     if (hiddenSubmitButton) {
@@ -97,19 +102,33 @@ const NavBar: React.FC = () => {
           </SheetTrigger>
           <SheetContent>
             <SheetHeader>
-              <SheetTitle>Create Profile</SheetTitle>
+              <SheetTitle>
+                {isCreatingAccount ? "Create Profile" : "Login"}
+              </SheetTitle>
               <SheetDescription>
-                Fill in the details below to create your profile.
+                {isCreatingAccount
+                  ? "Fill in the details below to create your profile."
+                  : "Log in to your account."}
               </SheetDescription>
             </SheetHeader>
-            <CreateAccount />
-            <SheetFooter>
+            {isCreatingAccount ? <CreateAccount /> : <LogIn />}
+            <SheetFooter className="mb-4">
               <SheetClose asChild>
                 <Button onClick={handleClickSubmit}>Submit</Button>
               </SheetClose>
             </SheetFooter>
-            <p>Or Login</p>
-            <Button>Login</Button>
+            <hr />
+            <div style={{ textAlign: "center", marginTop: "1rem" }}>
+              {isCreatingAccount ? (
+                <Button variant="outline" onClick={toggleForm}>
+                  Login
+                </Button>
+              ) : (
+                <Button variant="outline" onClick={toggleForm}>
+                  Sign up
+                </Button>
+              )}
+            </div>
           </SheetContent>
         </Sheet>
       </div>
