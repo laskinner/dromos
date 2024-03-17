5c4990afdf64254070c913ea5a9308a3c55ddaa6
Luke Skinner <laskinner@gmail.com>
2024-03-18 00:02:43 +0100

    Changed content-type to application/json

--- src/api/axiosDefaults.ts
+++ src/api/axiosDefaults.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://dromos-backend-1542a6a0bcb1.herokuapp.com";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 
 // Interceptor to include the token in every request
