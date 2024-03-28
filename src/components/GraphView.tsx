102d365abb26d5817c3f902aea411a0349d0281a
Luke Skinner <laskinner@gmail.com>
2024-03-28 18:10:41 +0100

    Added create node button and dialogue

--- src/components/GraphView.tsx
+++ src/components/GraphView.tsx
@@ -118,11 +118,7 @@ const GraphView: React.FC = () => {
                   <Label htmlFor="name" className="text-right">
                     Name
                   </Label>
-                  <Input
-                    id="name"
-                    value="Pedro Duarte"
-                    className="col-span-3"
-                  />
+                  <Input id="name" value="Graph Name" className="col-span-3" />
                 </div>
                 <div className="grid grid-cols-4 items-center gap-4">
                   <Label htmlFor="content" className="text-right">
@@ -136,7 +132,41 @@ const GraphView: React.FC = () => {
                 </div>
                 <div>
                   <Switch id="is_public" />
-                  <Label htmlFor="is_public">Private</Label>
+                  <Label htmlFor="is_public"> Private</Label>
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Create Node</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Create Node</DialogTitle>
+                <DialogDescription>
+                  Add new node details here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="title" className="text-right">
+                    Name
+                  </Label>
+                  <Input id="title" value="Node Name" className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="content" className="text-right">
+                    Description
+                  </Label>
+                  <Textarea
+                    id="content"
+                    value="Description"
+                    className="col-span-3"
+                  />
                 </div>
               </div>
               <DialogFooter>
