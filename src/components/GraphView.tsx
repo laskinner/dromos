6486f866558192976c5be47ca21c51f28eba49c7
Luke Skinner <laskinner@gmail.com>
2024-03-28 17:43:11 +0100

    Added create graph to graphs view

--- src/components/GraphView.tsx
+++ src/components/GraphView.tsx
@@ -3,12 +3,27 @@ import axios from "axios";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import NodeGraphView from "@/components/NodeGraphView";
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Switch } from "@/components/ui/switch";
 
 // Interface for an area
 export interface Area {
   id: string;
   name: string;
-  image: string; // Assuming each area has an image URL
+  image: string;
+  is_user_area: boolean;
   // Add more fields as necessary
 }
 
@@ -86,6 +101,50 @@ const GraphView: React.FC = () => {
             ))}
           </div>
         </ScrollArea>
+        <div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Create Graph</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Create Graph</DialogTitle>
+                <DialogDescription>
+                  Add new graph details here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Name
+                  </Label>
+                  <Input
+                    id="name"
+                    value="Pedro Duarte"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="content" className="text-right">
+                    Description
+                  </Label>
+                  <Textarea
+                    id="content"
+                    value="Description"
+                    className="col-span-3"
+                  />
+                </div>
+                <div>
+                  <Switch id="is_public" />
+                  <Label htmlFor="is_public">Private</Label>
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
       </aside>
       <main className="flex-1 p-4">
         {selectedAreaId && <NodeGraphView areaId={selectedAreaId} />}
