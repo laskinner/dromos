559335502261cc179e7dd11b8ae93edf8bf5457b
Luke Skinner <laskinner@gmail.com>
2024-04-12 14:00:21 +0200

    Removed popover functionality, simplified and fixed commond component for causeSelector

--- src/components/CauseSelector.tsx
+++ src/components/CauseSelector.tsx
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAreaStore } from "@/stores/useAreaStore";
-import { Check, ChevronsUpDown } from "lucide-react";
 import {
   Command,
   CommandItem,
   CommandEmpty,
+  CommandGroup,
   CommandInput,
+  CommandList,
 } from "@/components/ui/command";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@/components/ui/popover";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+// import { cn } from "@/lib/utils";
 import { NodeData } from "@/lib/interfaces/graphTypes";
 
 interface GraphData {
@@ -25,7 +20,6 @@ export const CauseSelector: React.FC<{
   selectedCauses: string[];
   onSelectionChange: (nodeId: string) => void;
 }> = ({ selectedCauses, onSelectionChange }) => {
-  const [open, setOpen] = useState(false);
   const selectedAreaId = useAreaStore((state) => state.selectedAreaId);
   const [graphData, setGraphData] = useState<GraphData>({ nodes: [] });
 
@@ -55,51 +49,32 @@ export const CauseSelector: React.FC<{
   }, [selectedAreaId]);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {selectedCauses.length > 0
-            ? graphData.nodes
-                .filter((node) => selectedCauses.includes(node.id))
-                .map((node) => node.title)
-                .join(", ")
-            : "Select Nodes..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="Search nodes..." />
-          {graphData.nodes.length ? (
-            graphData.nodes.map((node) => (
-              <CommandItem
-                key={node.id}
-                onSelect={() => {
-                  onSelectionChange(node.id);
-                  setOpen(false);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2",
-                    selectedCauses.includes(node.id)
-                      ? "opacity-100"
-                      : "opacity-0",
-                  )}
-                />
-                {node.title}
-              </CommandItem>
-            ))
-          ) : (
-            <CommandEmpty>No nodes found.</CommandEmpty>
-          )}
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <>
+      <Command>
+        <CommandInput placeholder="Search nodes..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            {graphData.nodes.length ? (
+              graphData.nodes.map((node) => (
+                <CommandItem
+                  key={node.id}
+                  onSelect={() => {
+                    onSelectionChange(node.id);
+                  }}
+                >
+                  {node.title}
+                </CommandItem>
+              ))
+            ) : (
+              <CommandEmpty>No nodes found.</CommandEmpty>
+            )}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+      <br />
+      <h1>Selected Causes</h1>
+      <p>{selectedCauses}</p>
+    </>
   );
 };
