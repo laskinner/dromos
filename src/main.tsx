6eb140b909e6b695453ded0de1dcc0663b2013fe
Luke Skinner <laskinner@gmail.com>
2024-05-17 17:38:20 +0200

    Fixed bad import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import "./axiosDefaults";
+import "./api/axiosDefaults";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
