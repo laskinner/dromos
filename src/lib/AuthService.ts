a7439fe70c4a2030549a7fb5e3e780b662f1acbc
Luke Skinner <laskinner@gmail.com>
2024-05-27 21:07:56 +0200

    Added additional functionlity to completely clear browsing data, cookies, and cash to fix CSRF issue

--- src/lib/AuthService.ts
+++ src/lib/AuthService.ts
@@ -51,11 +51,26 @@ const logout = (): void => {
   localStorage.removeItem("refreshToken");
 
   // Clear cookies
-  document.cookie.split(";").forEach((c) => {
-    document.cookie = c
-      .replace(/^ +/, "")
-      .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-  });
+  const cookies = document.cookie.split("; ");
+  for (const cookie of cookies) {
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+  }
+
+  // Clear session storage
+  sessionStorage.clear();
+
+  // Clear indexedDB
+  if (window.indexedDB) {
+    window.indexedDB.databases().then((databases) => {
+      databases.forEach((db) => {
+        if (db.name) {
+          window.indexedDB.deleteDatabase(db.name);
+        }
+      });
+    });
+  }
 
   // Delete authorization header
   delete axios.defaults.headers.common["Authorization"];
